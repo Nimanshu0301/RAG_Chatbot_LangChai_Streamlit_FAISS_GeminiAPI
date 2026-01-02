438fc9db53716206536bd0258f3865c957625ff8
Nimanshu0301 <nimanshu312@gmail.com>
2026-01-01 20:55:12 -0600

    function for conversation chain

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain_google_genai import GoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 
 from datetime import datetime
@@ -37,3 +36,31 @@ def get_text_chunks(text, model_name):
     )
     chunks = text_splitter.split_text(text)
     return chunks
+
+#embedding this chunks and storing in vector db 
+def get_vectorstore(text_chunks, model_name, api_key=None):
+    if model_name == "Google AI":
+        embeddings = GoogleGenerativeAIEmbeddings(model="mode1/embedding-001", google_api_key=api_key)
+        
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    vectorstore.save_local("faiss_index")
+    return vectorstore
+
+# create a conversation chain using langchain
+def  get_conversation_chain(model_name, vectorstore=None, api_key=None):
+    if model_name == "Google AI":
+       
+        prompt_template = """Answer the question as detailed as possible from the provided context, 
+        make sure to provide all the details with proper structure, if the answer is not in the provided context just say, 
+        "answer is not available in the context", don't provide wrong answer.
+        Context: {context}
+        Question: {question}
+        Answer in a concise manner."""
+
+        model = GoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3, google_api_key=api_key)
+        PROMPT = PromptTemplate(
+            template=prompt_template, input_variables=["context", "question"]
+        )
+        
+        chain = load_qa_chain(model, chain_type="stuff", prompt=PROMPT)
+        return chain
