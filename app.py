f30990f3b40939ea5de945a9979e406689fe42b7
Nimanshu0301 <nimanshu312@gmail.com>
2026-01-01 21:22:17 -0600

    Main function

--- app.py
+++ app.py
@@ -149,4 +149,80 @@ if len(st.session_state.conversation_history) > 0:
         st.markdown("To download the conversation, click the Download button on the left side at the bottom of the conversation.")
 st.snow()
 
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs (v1) :books:")
 
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
+    linkedin_profile_link = "https://www.linkedin.com/in/snsupratim/"
+    kaggle_profile_link = "https://www.kaggle.com/snsupratim/"
+    github_profile_link = "https://github.com/snsupratim/"
+
+    st.sidebar.markdown(
+        f"[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)]({linkedin_profile_link}) "
+        f"[![Kaggle](https://img.shields.io/badge/Kaggle-20BEFF?style=for-the-badge&logo=kaggle&logoColor=white)]({kaggle_profile_link}) "
+        f"[![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)]({github_profile_link})"
+    )
+
+
+
+    model_name = st.sidebar.radio("Select the Model:", ( "Google AI"))
+
+    api_key = None
+
+    if model_name == "Google AI":
+        api_key = st.sidebar.text_input("Enter your Google API Key:")
+        st.sidebar.markdown("Click [here](https://ai.google.dev/) to get an API key.")
+        
+        if not api_key:
+            st.sidebar.warning("Please enter your Google API Key to proceed.")
+            return
+
+   
+    with st.sidebar:
+        st.title("Menu:")
+        
+        col1, col2 = st.columns(2)
+        
+        reset_button = col2.button("Reset")
+        clear_button = col1.button("Rerun")
+
+        if reset_button:
+            st.session_state.conversation_history = []  # Clear conversation history
+            st.session_state.user_question = None  # Clear user question input 
+            
+            
+            api_key = None  # Reset Google API key
+            pdf_docs = None  # Reset PDF document
+            
+        else:
+            if clear_button:
+                if 'user_question' in st.session_state:
+                    st.warning("The previous query will be discarded.")
+                    st.session_state.user_question = ""  # Temizle
+                    if len(st.session_state.conversation_history) > 0:
+                        st.session_state.conversation_history.pop()  # Son sorguyu kaldır
+                else:
+                    st.warning("The question in the input will be queried again.")
+
+
+
+
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            if pdf_docs:
+                with st.spinner("Processing..."):
+                    st.success("Done")
+            else:
+                st.warning("Please upload PDF files before processing.")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question, model_name, api_key, pdf_docs, st.session_state.conversation_history)
+        st.session_state.user_question = ""  # Clear user question input 
+
+if __name__ == "__main__":
+    main()     
+    
